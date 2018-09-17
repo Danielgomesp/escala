77d105c567b8cad4dfe67778b7e7d69cc7f893f2
danielgomesp <danielgomesp@gmail.com>
2018-09-17 11:04:14 -0300

    Apaguei espaços em brancos para testar o VS.

--- cadastrar_colaborador.php
+++ cadastrar_colaborador.php
@@ -89,10 +89,8 @@
                             </div>
                         </div>
                     </div>
-
                 </form>
             </div>
-
         </div>
     </div>
 </div>
